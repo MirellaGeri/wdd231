83b71b065697edba38b82415604d92f77c3beef0
wddbyuidaho <mmirellaisabel@yahoo.es>
2024-10-07 16:35:36 -0500

    update index

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,77 +1,61 @@
-const companies = `[
-    {
-      "names": "Alcampo SAC",
-      "addresses": "Calle molinos 101",
-      "phonenumbers": "636363663",
-      "website URL": "https://www.compraonline.alcampo.es/",
-      "image": "Alcampo.png",
-      "membershiplevel": "1",
-      "otherinformation": null
-    },
-    {
-      "names": "Carrefour SAC",
-      "addresses": "Calle lopez de albujar 12",
-      "phonenumbers": "6363888863",
-      "website URL": "https://www.carrefour.es/",
-      "image": "Carrefour.png",
-      "membershiplevel": "2",
-      "otherinformation": null
-    },
-    {
-      "names": "DIA SAC",
-      "addresses": "Calle bravo murillo 13",
-      "phonenumbers": "636363663",
-      "website URL": "https://www.dia.es/",
-      "image": "Dia.png",
-      "membershiplevel": "3",
-      "otherinformation": null
-    },
-    {
-      "names": "Ahorramas SAC",
-      "addresses": "Calle nuestra señora del carmen 35",
-      "phonenumbers": "636367778",
-      "website URL": "https://www.ahorramas.com/",
-      "image": "Ahorramas.png",
-      "membershiplevel": "1",
-      "otherinformation": null
-    },
-    {
-      "names": "Aldi SAC",
-      "addresses": "Calle recoletos 36",
-      "phonenumbers": "636368863",
-      "website URL": "https://www.aldi.es/",
-      "image": "Aldi.png",
-      "membershiplevel": "2",
-      "otherinformation": null
-    },
-    {
-      "names": "Lidl SAC",
-      "addresses": "Calle alvarado 11",
-      "phonenumbers": "636853663",
-      "website URL": "https://www.lidl.es/",
-      "image": "Lidl.png",
-      "membershiplevel": "3",
-      "otherinformation": null
-    },
-    {
-      "names": "Mercadona SAC",
-      "addresses": "Calle lope de aro 2",
-      "phonenumbers": "636362453",
-      "website URL": "https://www.mercadona.es/",
-      "image": "Mercadona.png",
-      "membershiplevel": "3",
-      "otherinformation": null
-    }
-  ]`;
-const jsonData = JSON.parse(companies);
 
-displayCard(companies);
+import members from '../data/members.json' with {type:'json'};
+
+let homegrid2 = document.querySelector('.home-grid2');
+
+members.forEach(element => {
+  homegrid2.innerHTML += `<section class="card2">
+                                <h2>${element.names}</h2>
+                                <div class="business">
+                                  <img class="logo_business" src="images/logo-business.png" >
+                                    <div id="text_business">
+                                        <p>Email: ${element.address}</p>
+                                        <p>phone: ${element.phone}</p>
+                                        <p>URL: <a href="${element.website}target="_blank">${element.website}</a></p>
+                                        <p>Membership Level: ${element.membershiplevel}</p>
+                                    </div>
+                                </div>
+                            </section>`;
+});
+
+/*
+displayCard(members);
 
 function displayCard(companiesfiltered) {
-  document.getElementById("card").innerHTML = ""
-  companiesfiltered.forEach(companie => {
-      const companiesCard = document.createElement("section")
-      companiesCard.innerHTML = `<section><h3>${temple}</h3><p>Location: ${temple.location}</p><p>Dedicated: ${temple.dedicated}</p><p>Area: ${temple.area}</p><img src=${temple.imageUrl} alt=${temple.templeName} loading="lazy"></section>`;
-      document.getElementById("card").appendChild(templeCard);
+  document.getElementById("card").innerHTML = ''
+  companiesfiltered.forEach( member => {
+  const companiesCard = document.createElement("section")
+  companiesCard.innerHTML = `<section class="card2">
+                                <h2>${member.name}</h2>
+                                <h3>Business Tag Line</h3>
+                                <div class="business">
+                                    <img class="logo_business" src="./images/${member.image} alt=${member.name}" >
+                                    <div id="text_business">
+                                        <p>Email: ${member.address}</p>
+                                        <p>phone: ${member.phone}</p>
+                                        <p>URL: <a href="${member.website}target="_blank">${member.website}</a></p>
+                                        <p>Membership Level: ${member.membershiplevel}</p>
+                                    </div>
+                                </div>
+                            </section>`;
+  document.getElementById("card").appendChild(companiesCard);
 });
-}
+}*/
+
+/*memberCard.innerHTML = `
+<img src="./images/${member.icon}" alt="${member.name}">
+<h2>${member.name}</h2>
+<p>${member.address}</p>
+<p>${member.phone}</p>
+<p><a href="${member.website}" target="_blank">${member.website}</a></p>
+<p>Membership Level: ${member.membershipLevel}</p>
+<p>${member.additionalInfo}</p>
+`;
+
+<img class="logo_business" src="./images/${element.image} alt=${element.names}" >
+
+const companiesCard = document.createElement("section")
+      companiesCard.innerHTML = `<section><h3>${mem}</h3><p>Location: ${temple.location}</p><p>Dedicated: ${temple.dedicated}</p><p>Area: ${temple.area}</p><img src=${temple.imageUrl} alt=${temple.templeName} loading="lazy"></section>`;
+      document.getElementById("card").appendChild(templeCard);
+
+membersContainer.appendChild(memberCard);*/
